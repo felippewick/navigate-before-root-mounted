a726fb248096e9f98010b33e7874505722410323
fmwick <49169290+felippewick@users.noreply.github.com>
2024-09-04 10:10:18 +0200

    add AppLayout

---
+++ app/(app)/_layout.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Slot, SplashScreen, router } from "expo-router";
+import { useFonts } from "expo-font";
+import { Text } from "react-native";
+export default function AppLayout() {
+  const [loaded] = useFonts({
+    // SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  React.useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  //   React.useEffect(() => {
+  //     router.push("/about");
+  //   }, []);
+
+  // It is OK to defer rendering this nested layout's content. We couldn't
+  // defer rendering the root layout's content since a navigation event (the
+  // redirect) would have been triggered before the root layout's content had
+  // been mounted.
+  if (!loaded) {
+    return <Text>Loading...</Text>;
+  }
+
+  return <Slot />;
+}
